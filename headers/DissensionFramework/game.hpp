08c8bd04160492a1f7636a4d876d67bcce74016d
PouPeuu <qwertydude277@gmail.com>
2025-02-15 13:28:19 +0200

    Create main game loop

--- headers/DissensionFramework/game.hpp
+++ headers/DissensionFramework/game.hpp
@@ -2,8 +2,12 @@
 #define GAME_H
 
 #include <string>
+#include <vector>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <DissensionFramework/game_object.hpp>
 
 
 namespace DissensionFramework {
@@ -12,12 +16,18 @@ namespace DissensionFramework {
             int _window_width, _window_height;
             std::string _window_title;
 
+            std::vector<GameObject*> _game_objects;
+
             static void _setFramebufferSize(GLFWwindow* window, int width, int height);
             void _processInput(GLFWwindow* window);
+
+            void _main();
         public:
             Game(int window_width, int window_height, std::string window_title = "DissensionFramework");
 
             void run();
+
+            void addGameObject(GameObject* game_object);
     };
 }
 
--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,7 @@
 #include <DissensionFramework/game.hpp>
 
 #include <stdexcept>
+#include <algorithm>
 
 void DissensionFramework::Game::_setFramebufferSize(GLFWwindow* window, int width, int height) {
     Game* game = static_cast<Game*>(glfwGetWindowUserPointer(window));
@@ -19,6 +20,34 @@ void DissensionFramework::Game::_processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
+
+void DissensionFramework::Game::_main() {
+    std::vector<unsigned int> deletion_queue;
+
+    for (unsigned int i = 0; i < _game_objects.size(); i++) {
+        GameObject* game_object = _game_objects[i];
+
+        State return_state = game_object->update();
+        if (return_state == QUEUE_DELETE)
+            deletion_queue.push_back(i);
+    }
+
+    for (unsigned int i : deletion_queue) {
+        std::swap(_game_objects[i], _game_objects[_game_objects.size() - 1]);
+        _game_objects.pop_back();
+    }
+
+    for (unsigned int i = 0; i < _game_objects.size(); i++) {
+        GameObject* game_object = _game_objects[i];
+        game_object->draw();
+    }
+}
+
+void DissensionFramework::Game::addGameObject(GameObject* game_object) {
+    _game_objects.push_back(game_object);
+}
+
 
 void DissensionFramework::Game::run() {
     glfwInit();
@@ -43,6 +72,8 @@ void DissensionFramework::Game::run() {
 
     while (!glfwWindowShouldClose(window)) {
         _processInput(window);
+        
+        _main();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
