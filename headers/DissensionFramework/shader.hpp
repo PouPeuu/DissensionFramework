ee788e5aaf69d801760e1ef6722388017b5e6e63
PouPeuu <qwertydude277@gmail.com>
2025-02-20 15:30:02 +0200

    Create shader class

--- headers/DissensionFramework/shader.hpp
+++ headers/DissensionFramework/shader.hpp
@@ -1,6 +1,21 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-void testkakka();
+#include <DissensionFramework/resource.hpp>
+// This is opengl only for now...
+#include <DissensionFramework/opengl_includes.hpp>
+
+namespace DissensionFramework {
+    class Shader {
+        private:
+            int _id;
+
+            unsigned int _compileShader(Resource shader_resource, GLuint shader_type);
+        public:
+            Shader(Resource vertex_shader_resource, Resource fragment_shader_resource);
+
+            void use();
+    };
+}
 
 #endif
--- src/shader.cpp
+++ src/shader.cpp
@@ -2,6 +2,48 @@
 
 #include <iostream>
 
-void testkakka() {
-    printf("Testkakka'd\n");
+unsigned int DissensionFramework::Shader::_compileShader(Resource shader_resource, GLuint shader_type) {
+    unsigned int shader;
+
+    shader = glCreateShader(shader_type);
+    
+    const char* shader_source = (const char*)shader_resource.getData().data();
+    const int size = (unsigned int)shader_resource.getSize();
+    glShaderSource(shader, 1, &shader_source, &size);
+
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cerr << "Shader " << shader_resource.getRealPath() << " compilation failed:\n" << infoLog << std::endl;
+    }
+
+    return shader;
+}
+
+DissensionFramework::Shader::Shader(Resource vertex_shader_resource, Resource fragment_shader_resource) {
+    _id = glCreateProgram();
+    unsigned int vertex_shader = _compileShader(vertex_shader_resource, GL_VERTEX_SHADER);
+    glAttachShader(_id, vertex_shader);
+    unsigned int fragment_shader = _compileShader(fragment_shader_resource, GL_FRAGMENT_SHADER);
+    glAttachShader(_id, fragment_shader);
+    glLinkProgram(_id);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(_id, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(_id, 512, NULL, infoLog);
+        std::cerr << "Shader program linking failed:\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+void DissensionFramework::Shader::use() {
+    glUseProgram(_id);
 }
