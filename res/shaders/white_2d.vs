99ae5a0e7d97335fbc1ba0d1c25f86e1559088b3
PouPeuu <qwertydude277@gmail.com>
2025-02-22 14:29:58 +0200

    Fix rendering

--- res/shaders/white_2d.vs
+++ res/shaders/white_2d.vs
@@ -1,9 +1,9 @@
 #version 330 core
 layout (location = 0) in vec2 pos;
 
-out vec4 vertex_color;
+out vec4 color;
 
 void main() {
     gl_Position = vec4(pos.xy, 0.0, 1.0);
-    vertex_color = vec4(1.0, 1.0, 1.0, 1.0);
+    color = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- src/opengl_renderer.cpp
+++ src/opengl_renderer.cpp
@@ -14,12 +14,12 @@ void DissensionFramework::OpenGLRenderer::drawPolygon(std::vector<glm::vec2> poi
         vertices[2 * i + 1] = points[i].y;
     }
 
-    unsigned int indices_size = (vertices_size - 2) * 3;
+    unsigned int indices_size = (points.size() - 2) * 3;
     unsigned int* indices = new unsigned int[indices_size];
-    for (unsigned int i = 0; i < vertices_size - 2; i++) {
-        indices[i] = 0;
-        indices[i + 1] = i + 1;
-        indices[i + 2] = i + 2;
+    for (unsigned int i = 0; i < points.size() - 2; i++) {
+        indices[3 * i] = 0;
+        indices[3 * i + 1] = i + 1;
+        indices[3 * i + 2] = i + 2;
     }
 
     unsigned int VAO;
@@ -36,14 +36,16 @@ void DissensionFramework::OpenGLRenderer::drawPolygon(std::vector<glm::vec2> poi
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices_size, vertices, GL_DYNAMIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * indices_size, indices, GL_DYNAMIC_DRAW);
     
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     Shaders::WHITE_2D.use();
 
     glDrawElements(GL_TRIANGLES, indices_size, GL_UNSIGNED_INT, 0);
-}
 
-void DissensionFramework::OpenGLRenderer::drawEquilateralPolygon(unsigned int count, float radius, Color color) const {
-    
+    delete[] vertices;
+    delete[] indices;
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 }
