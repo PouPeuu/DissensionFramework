84a9255c2d9552d3d19c1430f559f20a1ccdce36
PouPeuu <qwertydude277@gmail.com>
2025-02-23 10:52:35 +0200

    Add equilateral polygon rendering and color

--- res/shaders/color_2d.vs
+++ res/shaders/color_2d.vs
@@ -1,10 +1,10 @@
 #version 330 core
 layout (location = 0) in vec2 pos;
-layout (location = 1) in vec4 color;
+layout (location = 1) in vec4 in_color;
 
-out vec4 vertex_color;
+out vec4 color;
 
 void main() {
     gl_Position = vec4(pos.xy, 0.0, 1.0);
-    vertex_color = color;
+    color = in_color;
 }
--- src/opengl_renderer.cpp
+++ src/opengl_renderer.cpp
@@ -7,11 +7,15 @@ void DissensionFramework::OpenGLRenderer::clear() const {
 }
 
 void DissensionFramework::OpenGLRenderer::drawPolygon(std::vector<glm::vec2> points, Color color) const {
-    unsigned int vertices_size = points.size() * 2;
+    unsigned int vertices_size = points.size() * 6;
     float* vertices = new float[vertices_size];
     for (unsigned int i = 0; i < points.size(); i++) {
-        vertices[2 * i] = points[i].x;
-        vertices[2 * i + 1] = points[i].y;
+        vertices[6 * i] = points[i].x;
+        vertices[6 * i + 1] = points[i].y;
+        vertices[6 * i + 2] = color.r;
+        vertices[6 * i + 3] = color.g;
+        vertices[6 * i + 4] = color.b;
+        vertices[6 * i + 5] = color.a;
     }
 
     unsigned int indices_size = (points.size() - 2) * 3;
@@ -36,10 +40,12 @@ void DissensionFramework::OpenGLRenderer::drawPolygon(std::vector<glm::vec2> poi
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices_size, vertices, GL_DYNAMIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * indices_size, indices, GL_DYNAMIC_DRAW);
     
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(2*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-    Shaders::WHITE_2D.use();
+    Shaders::COLOR_2D.use();
 
     glDrawElements(GL_TRIANGLES, indices_size, GL_UNSIGNED_INT, 0);
 
@@ -48,4 +54,16 @@ void DissensionFramework::OpenGLRenderer::drawPolygon(std::vector<glm::vec2> poi
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+}
+
+void DissensionFramework::OpenGLRenderer::drawEquilateralPolygon(unsigned int count, float radius, Color color) const {
+    std::vector<glm::vec2> points;
+
+    float interval = M_PI * 2 / count;
+    for (unsigned int i = 0; i < count; i++) {
+        float angle = interval * i;
+        points.push_back(glm::vec2(cos(angle), sin(angle)) * radius);
+    }
+
+    drawPolygon(points, color);
 }
